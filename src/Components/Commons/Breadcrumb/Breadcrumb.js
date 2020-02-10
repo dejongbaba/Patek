2d0b3e9e2dc8028d731c97a3d75e7e907cb7a782
Agunbiade Adedeji <agunbiade.adedeji94@gmail.com>
2020-02-10 14:47:54 +0100

    fixed breadcrumb error

--- src/Components/Commons/Breadcrumb/Breadcrumb.js
+++ src/Components/Commons/Breadcrumb/Breadcrumb.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import './breadcrumb.css';
+
 const Breadcrumb = ({links, linkClass = '', containerClass = 'mb-lg-3'}) => {
 
     //TODO handle active link
     const currentPath = window.location.pathname;
 
     return (
-        <div className={containerClass +' nav-breadcrumb'}>
+        <div className={containerClass + ' nav-breadcrumb'}>
             {links && links.length > 0 ?
                 links.map((link, i) => {
                     return (i === links.length - 1) ?
-                        <a key={i} className={`text-uppercase ${linkClass} ${link.path === currentPath ? 'text-white font-weight-bold' : 'text-gray'}`}
-                              href={link.path}>{link.name}</a> :
-                        <>
-                            <a key={i} className={`text-uppercase text-gray ${linkClass}`}
-                                  href={link.path}>{link.name}
-                            </a>
-                            &nbsp;<span className={'text-white mx-lg-1'}>|</span>&nbsp;
-                        </>
-
+                        <a key={i}
+                           className={`text-uppercase ${linkClass} ${link.path === currentPath ? 'text-white font-weight-bold' : 'text-gray'}`}
+                           href={link.path}>{link.name}</a> :
+                            <span key={i}>
+                                <a className={`text-uppercase text-gray ${linkClass}`} href={link.path}>{link.name}
+                                </a>&nbsp;<span key={i} className={'text-white mx-lg-1'}>|</span>&nbsp;
+                            </span>
                 }) : ''
             }
         </div>
